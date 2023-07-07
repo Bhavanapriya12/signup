3039e83aa9f9812d3fec1b697bda163e802d3543
Bhavana <email goes here>
2023-07-07 14:53:04 +0530

    Few changes

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI,HTTPException
-from pydantic import BaseModel,validator,Field
+from pydantic import BaseModel,Field,EmailStr,validator
 from database import engine, SessionLocal
 import models
 from models import User
+from fastapi.middleware.cors import CORSMiddleware
 import re
 
 app = FastAPI()
+
+origins =[
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,14 +27,20 @@ models.Base.metadata.create_all(bind=engine)
 
 class UserCreate(BaseModel):
     username:str
-    password:str=Field(min_length=8)
-    email:str
+    password:str=Field(min_length=10)
 
-    @validator('email')
-    def validate_email(email):
-        if not email.endswith('@gmail.com'):
-            raise ValueError('only Gmail addresses are allowed')
-        return email
+    @validator('password')
+    def validate_password(cls,value):
+        special_chars=r"[!@#$%^&*()<>.,?/':;|']"
+        if not re.search(special_chars,value):
+            raise ValueError("password must contain atleast one special character")
+        if not any(char.isupper() for char in value):
+            raise ValueError("password must contain atleast one uppercase character")
+        if not any(char.islower() for char in value):
+            raise ValueError("password must contain atleast one lowercase character")
+        return value
+
+    email:EmailStr
 
 
 class UserLogin(BaseModel):
@@ -45,7 +65,6 @@ async def signup(user: UserCreate):
 @app.post("/login")
 async def login(user: UserLogin):
     db=SessionLocal()
-
     existing_user=db.query(User).filter(User.username==user.username).first()
     if not user or not verify_password(user.password,existing_user.password):
         raise HTTPException(status_code=401,detail="Incorrect username or password")
